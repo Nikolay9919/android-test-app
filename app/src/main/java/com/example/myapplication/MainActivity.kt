daf9b40ab2b5e9960a7828845e243a6b268f97a1
Nikolay kuzichev <kuzkuz51@gmail.com>
2024-04-09 21:58:15 +0300

    Added Dagger Hilt

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
@@ -12,14 +13,30 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
+import androidx.lifecycle.lifecycleScope
+import com.example.myapplication.domain.usecases.AppEntryUseCases
 import com.example.myapplication.presentation.onboarding.OnBoardingScreen
 import com.example.myapplication.ui.theme.MyApplicationTheme
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var appEntryUseCases: AppEntryUseCases
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
         installSplashScreen()
+        lifecycleScope.launch {
+            appEntryUseCases.readAppEntry().collect {
+                Log.d("Test", it.toString())
+            }
+        }
+
         setContent {
             MyApplicationTheme {
                 Box(modifier = Modifier.background(color = MaterialTheme.colorScheme.background)) {
