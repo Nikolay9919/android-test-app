9527f8914a205ccdfe29f23a1ba39315559e111d
Nikolay kuzichev <kuzkuz51@gmail.com>
2024-04-12 13:12:48 +0300

    Missed Constants in class, added api key and base url

--- app/src/main/java/com/example/myapplication/util/Constants.kt
+++ app/src/main/java/com/example/myapplication/util/Constants.kt
@@ -5,4 +5,8 @@ object Constants {
     const val USER_SETTINGS = "USER_SETTINGS"
 
     const val APP_ENTRY = "APP_ENTRY"
+
+    const val API_KEY = "3a1e0dccaaab4deea28f9d68a7b15e4d"
+
+    const val BASE_URL = "https://newsapi.org/v2/"
 }
